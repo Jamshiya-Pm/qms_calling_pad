0dcdb38ebd9ea7118481c24042b6505aef4141eb
DThree123 <jamshiya@dthree.co>
2026-08-11 15:24:32 +0300

    Deploy fresh Flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "58580403" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4116432889" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
